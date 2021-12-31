8fa91e934699e45b42add4d8d0780f39e9c3e34b
maha <mmahaece07@gmail.com>
2021-12-31 17:16:15 +0530

    Program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SerializationandDeserializationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /*Binary_Serialization binary_Serialization = new Binary_Serialization();
+          //  binary_Serialization.Serialization();
+            binary_Serialization.Deserialization();*/
+
+            XMLDemo obj = new XMLDemo();
+            obj.XMLSerialization();
+            obj.XMLDeserialization();
+
+            JsonDemo json = new JsonDemo();
+            json.JsonSerialization();
+            json.Deserialization();
+        }
+    }
+}
